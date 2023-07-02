381241c795a077effe4a6fbd94a6ead4e3d61adf
avilla13 <xetox13@gmail.com>
2023-07-02 17:41:25 -0500

    Random joke generator works

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ var router = express.Router();
 const ROOT_URL = 'https://api.chucknorris.io/jokes';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chuck Norris Jokes' });
+router.get('/', async function(req, res, next) {
+  const joke = await fetch(`${ROOT_URL}/random`)
+  .then(response => response.json())
+  .then(joke => {
+    res.render('index', { title: 'Chuck Norris Jokes', joke });
+  })
 });
 
 
