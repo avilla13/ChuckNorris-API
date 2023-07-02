85ab4004202a78d6d6a8c184ace11cfacec23adb
avilla13 <xetox13@gmail.com>
2023-07-02 16:29:37 -0500

    Setup index views boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const ROOT_URL = 'https://api.chucknorris.io/jokes';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chuck Norris Jokes' });
 });
+
+
+
+
+
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var chucksRouter = require('./routes/chucks');
 
 var app = express();
 
@@ -13,14 +14,32 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Mount middleware into the middleware/request pipeline
+// app.use([starts with path], <middleware fn> [more optional <middleware fn>])
+
+// app.use(function(req, res, next) {
+//   console.log('Hello Alex');
+//   // Add a time property to the res.locals object
+//   // The time property will be accessible within the templates
+//   res.locals.time = new Date().toLocaleTimeString();
+//   next();
+// });
+
+// Log in the terminal the HTTP request info
 app.use(logger('dev'));
+// Processes data sent in the body of request, if it's json
 app.use(express.json());
+// Processes data sent in 'form' body of request.
+// It will create a propery on req.body for each <input>, <select> and/or <textarea>
+// in the <form>
 app.use(express.urlencoded({ extended: false }));
+// Add a cookies property for each cookie sent in the request
 app.use(cookieParser());
+// If the request is for static asset, returns the file to browser
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/chucks', chucksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
